9f0420039bbb964f594516e08848f069a583de87
socratesmb <53800580+socratesmb@users.noreply.github.com>
2022-10-25 11:34:24 -0500

    abrir, guardar y ver imagenes

---
+++ 01_read_view_save_img.py
@@ -0,0 +1,10 @@
+import cv2
+
+imagen = cv2.imread('perro-samurai.jpg') # Leer una imagen
+
+#cv2.imwrite('perro-samurai-gris.jpg', imagen) # Guardar imagen
+
+# Visualizar imange
+cv2.imshow('Test de salida', imagen)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
